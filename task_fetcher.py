89f38d4db4578ddaa8422d9ffb4ed5e93d932240
Tim Summerton-Brier <t.brier93@gmail.com>
2026-09-04 14:30:53 +0100

    Apply retries to all http and https requests

--- task_fetcher.py
+++ task_fetcher.py
@@ -36,10 +36,11 @@ class TaskFetcher:
         # This will only retry connection failures, if you want to retry on certain HTTP status codes you should
         # supply the status_forcelist parameter
         retries = Retry(
-            total=3,
+            total=5,
             backoff_factor=0.1,
         )
-        session.mount('', HTTPAdapter(max_retries=retries))
+        session.mount('http://', HTTPAdapter(max_retries=retries))
+        session.mount('https://', HTTPAdapter(max_retries=retries))
         
         response = session.get(endpoint_url)
         
